11b5b9bc2c5184747d356308fd404c4b30ef0b1a
nifbastiani <nicolefbastiani@gmail.com>
2026-08-05 21:49:23 -0300

    feat: cria estrutura de pacotes e classes Carro e Sistema

---
+++ src/br/com/meusistema/main/Sistema.java
@@ -0,0 +1,18 @@
+package br.com.meusistema.main;
+import br.com.meusistema.model.Carro;
+
+public class Sistema{
+    public static void main(String[] args){
+        System.out.println("Iniciando o sistema...");
+
+        Carro meuCarro = new Carro();
+        meuCarro.modelo = "Civic";
+        meuCarro.cor = "Prata";
+        meuCarro.buzinar();
+
+        Carro outroCarro = new Carro();
+        outroCarro.modelo = "Gol";
+        outroCarro.cor = "Preto";
+        outroCarro.buzinar();
+    }
+}
